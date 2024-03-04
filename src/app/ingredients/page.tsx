95fd84195c0504b1acfd850a029a83f3465651f6
dremmwell <dremmwell4>
2024-03-04 20:15:02 +0000

    fetching ingredients

--- src/app/ingredients/page.tsx
+++ src/app/ingredients/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
-    return <p>Ingredients</p>;
-  }
+import { fetchIngredient } from "@/lib/data";
+
+
+export default async function Page() {
+  const ingredientsData = await fetchIngredient();
+  console.log(ingredientsData);
+
+  return <p>Ingredients</p>;
+}
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,4 @@
+import { sql } from '@vercel/postgres';
 import {
     Ingredient,
     Recipe,
@@ -7,6 +8,14 @@ import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchIngredient () {
     noStore();
+    try {
+        console.log('Fetching ingredients data');
+        const data = await sql<Ingredient>`SELECT * FROM ingredients`;
+        console.log('Data fetch completed.');
+        return data.rows;
+    }
+    catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch ingredient data.');
+    }
 }
-
-
