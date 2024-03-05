1cc488c9bdf4ddcb988cb10db7a7bd132aa4d7d6
dremmwell <dremmwell4>
2024-03-05 19:36:13 +0000

    form layout

--- src/app/ingredients/Dialog.tsx
+++ src/app/ingredients/Dialog.tsx
@@ -10,45 +10,22 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { IngredientForm } from "./IngredientForm"
+import { Separator } from "@/components/ui/separator"
+import { ScrollArea } from "@/components/ui/scroll-area"
+
 
 export function DialogIngredients() {
     return (
       <Dialog>
         <DialogTrigger asChild>
-          <Button className="self-end">Add Ingredient</Button>
+          <Button className="self-end mt-4">Add Ingredient</Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you are done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input
-                id="name"
-                defaultValue="Pedro Duarte"
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="username" className="text-right">
-                Username
-              </Label>
-              <Input
-                id="username"
-                defaultValue="@peduarte"
-                className="col-span-3"
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
+        <DialogContent className="sm:max-w-[425px] overflow-auto">
+              <h3 className="text-lg font-medium">Add Ingredient</h3>
+              <p className="text-sm text-muted-foreground">Add a new ingredient to your list !</p>
+              <Separator /> 
+              <IngredientForm />
         </DialogContent>
       </Dialog>
     )
---
+++ src/app/ingredients/IngredientForm.tsx
@@ -0,0 +1,212 @@
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { cn } from "@/lib/utils"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+ 
+const ingredientFormSchema = z.object({
+  name: z.string().min(2, {
+    message: "Ingredient name must be at least 2 characters.",
+  }),
+})
+
+export function IngredientForm() {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof ingredientFormSchema>>({
+      resolver: zodResolver(ingredientFormSchema),
+      defaultValues: {
+        name: "",
+      },
+    })
+   
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof ingredientFormSchema>) {
+      // Do something with the form values.
+      // ✅ This will be type-safe and validated.
+      console.log(values)
+    }
+
+    return (
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="ingredient name..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Tabs defaultValue="per100g">
+            <TabsList className="mb-2">
+              <TabsTrigger value="per100g">Per 100g</TabsTrigger>
+              <TabsTrigger value="custom">Custom Measure</TabsTrigger>
+            </TabsList>
+            <TabsContent value="per100g">
+              <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Calories (g)</FormLabel>
+                  <FormControl>
+                    <Input placeholder="calories per 100g..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Proteins (g)</FormLabel>
+                  <FormControl>
+                    <Input placeholder="proteins per 100g..."{...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Carbs (g)</FormLabel>
+                  <FormControl>
+                    <Input placeholder="carbs per 100g..."{...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+              <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Fats (g)</FormLabel>
+                  <FormControl>
+                    <Input placeholder="fats per 100g..."{...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            </TabsContent>
+            <TabsContent value="custom">
+              <div className="flex gap-4">
+                <div className="w-5/6">
+                <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Measurement Type</FormLabel>
+                    <FormControl>
+                      <Input placeholder="item, pint, cup, tsp..."{...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              </div>
+                <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Measure Weight (g)</FormLabel>
+                    <FormControl>
+                      <Input placeholder="grams for one measure..."{...field} />
+                    </FormControl>
+                    <FormDescription>
+                    Please indicate how many grams your custom measure weights.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Calories (g)</FormLabel>
+                  <FormControl>
+                    <Input placeholder="calories per item..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Proteins (g)</FormLabel>
+                  <FormControl>
+                    <Input placeholder="proteins per item..."{...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Carbs (g)</FormLabel>
+                  <FormControl>
+                    <Input placeholder="carbs per item..."{...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+              <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Fats (g)</FormLabel>
+                  <FormControl>
+                    <Input placeholder="fats per item..."{...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            </TabsContent>
+          </Tabs>
+          <Button type="submit">Add</Button>
+        </form>
+      </Form>
+    )
+}
