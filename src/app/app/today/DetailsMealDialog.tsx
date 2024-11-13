186d25135378b42290efd8ab17d7b36ea462948d
dremmwell <dremmwell4>
2024-11-13 23:23:55 +0000

    details dialog setup

---
+++ src/app/app/today/DetailsMealDialog.tsx
@@ -0,0 +1,39 @@
+import React, { Dispatch, SetStateAction } from 'react';
+
+import { Button } from '@/components/ui/button';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { TimeLineMeal	 } from '@/app/types/definitions';
+
+export default function DetailsDialog({
+  meal,
+  isOpen,
+  setIsOpen
+}: {
+  meal: TimeLineMeal	
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}) {
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0 mb-3">{meal.title}</DialogTitle>
+          <DialogDescription className='flex flex-col gap-4'>
+            {meal.description?.split('\n').map((line, index) => (
+            <div key={index} className="text-sm">{line}</div>
+            ))}
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/app/app/today/TimelineItem.tsx
+++ src/app/app/today/TimelineItem.tsx
@@ -13,12 +13,12 @@ import { useState } from 'react'
 import { ArrowUpDown, MoreHorizontal, SquarePen, Trash2, ListCollapse } from "lucide-react"
 import IconMenu from "@/components/icon-menu";
 import DeleteMealDialog from './DeleteMealDialog';
+import DetailsDialog from './DetailsMealDialog';
 
 function TimelineItem( {meal} : {meal : TimeLineMeal} ) {
 
       // Handles Dioalog open/close state //
   const [isDetailsOpen, setIsDetailsOpen] = useState(false);
-  const [isEditOpen, setIsEditOpen] = useState(false);
   const [isDeleteOpen, setIsDeleteOpen] = useState(false);
 
 
@@ -29,10 +29,13 @@ function TimelineItem( {meal} : {meal : TimeLineMeal} ) {
             isOpen={isDeleteOpen}
             setIsOpen={setIsDeleteOpen}
         />
-
-
+        <DetailsDialog 
+            meal={meal}
+            isOpen={isDetailsOpen}
+            setIsOpen={setIsDetailsOpen}
+        />
         <li key={meal.mealId} className="last:mb-0 mb-2">
-            <div className="grid grid-cols-[minmax(60px,70px)_auto_1fr] grid-rows-[20px_auto] gap-3">
+            <div className="grid grid-cols-[minmax(60px,70px)_auto_1fr_auto] grid-rows-[20px_auto] gap-3">
             <div className="ml-auto">
                 <h3 className="text-sm font-semibold text-foreground">{meal.title}</h3>
             </div>
@@ -44,7 +47,7 @@ function TimelineItem( {meal} : {meal : TimeLineMeal} ) {
                 <div key={index} className="text-sm font-medium text-muted-foreground leading-relaxed -translate-y-[36px]">{line}</div>
                 ))}
             </div>
-            <div className="ml-auto col-start-1 row-start-2">
+            <div className="ml-auto col-start-4 row-start-1 row-span-2">
                 <DropdownMenu modal={false}> {/* modal={false} prevents instantly closing dialog modals */}
                 <DropdownMenuTrigger asChild>
                     <Button variant="ghost" className="h-8 w-8 p-0">
@@ -63,20 +66,6 @@ function TimelineItem( {meal} : {meal : TimeLineMeal} ) {
                         <IconMenu
                         text="Details"
                         icon={<ListCollapse className="h-4 w-4" />}
-                        />
-                    </button>
-                    </DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem className="group flex w-full items-center justify-between  text-left p-0 text-sm font-base text-neutral-500 ">
-                    <button
-                        onClick={() => {
-                        setIsEditOpen(true);
-                        }}
-                        className="w-full justify-start flex rounded-md p-2"
-                    >
-                        <IconMenu
-                        text="Edit"
-                        icon={<SquarePen className="h-4 w-4" />}
                         />
                     </button>
                     </DropdownMenuItem>
