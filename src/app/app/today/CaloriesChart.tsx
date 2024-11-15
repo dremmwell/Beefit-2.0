bb6dcd169c34e0d5a76941b66dd1008a0633c141
dremmwell <dremmwell4>
2024-11-15 11:20:50 +0000

    calories chart layout

--- src/app/app/today/CaloriesChart.tsx
+++ src/app/app/today/CaloriesChart.tsx
@@ -9,17 +9,17 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-export default function CaloriesChart() {
+export default function CaloriesChart({  }) {
 
   const radius = 90;
   const percent = 100;
-  const percentSegment = 10;
+  const percentSegment = 100;
 
-  const strokeWidth = radius * 0.16;
+  const strokeWidth = radius * 0.13;
   const innerRadius = radius - ( strokeWidth / 2 );
 
   const circumference = innerRadius * 2 * Math.PI;
-  const arc = circumference * (200 / 360);
+  const arc = circumference * (205 / 360);
   const dashArray = `${arc} ${circumference}`;
   const transform = `rotate(150, ${radius}, ${radius})`;
 
@@ -35,10 +35,10 @@ export default function CaloriesChart() {
 
   return (
     <Card >
-      <CardHeader >
-        <CardTitle>Calories :</CardTitle>
+      <CardHeader>
+        <CardTitle className="text-center">Calories</CardTitle>
       </CardHeader>
-      <CardContent >
+      <CardContent className="translate-y-2 pb-2" >
     <svg
       height={radius * 2}
       width={radius * 2}
@@ -96,11 +96,25 @@ export default function CaloriesChart() {
         transform={transformSegment}
      />
     </svg>
-      <div className="-translate-y-[20px] text-center flex flex-col">
-        <span>of 2200 cal</span>
-        <span>(200 cal left)</span>
-      </div>
+    <div className="absolute top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
+      <p className="fill-foreground text-4xl font-bold">
+          1800
+      </p>
+      <p className="fill-muted-foreground">
+          calories
+      </p>
+    </div>
       </CardContent>
+        <CardFooter className="flex-col gap-2 text-sm">
+          <div className="flex items-center gap-2 font-medium leading-none">
+            of 2200 cal
+          </div>
+          <div className="leading-none text-muted-foreground">
+            (200 cal left)
+          </div>
+        </CardFooter>
     </Card>
   )
 }
+
+
