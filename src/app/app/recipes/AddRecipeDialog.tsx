e77a8d2c4cc5bcbaf0600e3085e2930539e6b08b
dremmwell <dremmwell4>
2025-04-08 14:34:13 +0100

    fetching ingredients on Add Recipe Dialog

--- src/app/app/recipes/AddRecipeDialog.tsx
+++ src/app/app/recipes/AddRecipeDialog.tsx
@@ -27,59 +27,46 @@ import { Ingredient } from "@prisma/client"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import AddRecipeForm from "./AddRecipeForm"
 import { columns } from "./columns"
+import { getIngredients } from "@/app/actions/db.actions/ingredient.actions"
+import { User } from "lucia"
 
 
-export function AddRecipeDialog( {
-  ingredients
-}:{
-  ingredients: Array<Ingredient>
-}) {
+export function AddRecipeDialog( { user } : { user : User }) {
   const [open, setOpen] = useState(false)
   const isDesktop = useMediaQuery("(min-width: 768px)")
+
+  const [ingredients, setIngredients] = useState<Array<Ingredient>>()
+
+  useEffect(() => {
+    async function fetchIngredients() {
+    const data :Array<Ingredient> = await getIngredients(user.id);
+    setIngredients(data)
+    }
+    fetchIngredients()
+  }, [user.id])
 
   const handleCloseDialog = () => {
     setOpen(false)
   }
  
-  if (true) {
-    return (
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button >Create Recipe</Button>
-        </DialogTrigger>
-        <DialogContent className="min-w-fit min-h-[620px] max-h-full flex-1">
-          <DialogHeader >
-            <DialogTitle>New Recipe</DialogTitle>
-            <DialogDescription>
-              Create a new recipe and add it to your list!
-            </DialogDescription>
-          </DialogHeader>
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button >Create Recipe</Button>
+      </DialogTrigger>
+      <DialogContent className="min-w-fit min-h-[620px] max-h-full flex-1">
+        <DialogHeader >
+          <DialogTitle>New Recipe</DialogTitle>
+          <DialogDescription>
+            Create a new recipe and add it to your list!
+          </DialogDescription>
+        </DialogHeader>
+        {ingredients &&
           <ScrollArea className="overflow-y-scroll no-scrollbar">
             <AddRecipeForm columns={columns} data={ingredients} onSave={handleCloseDialog}/>
           </ScrollArea>
-        </DialogContent>
-      </Dialog>
-    )
-  }
- 
-  return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        <Button >Create Recipe</Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>New Recipe</DrawerTitle>
-          <DrawerDescription>
-            Create a new recipe and add it to your list!
-          </DrawerDescription>
-        </DrawerHeader>
-        <ScrollArea className="overflow-y-scroll no-scrollbar">
-          <AddRecipeForm columns={columns} data={ingredients} onSave={(handleCloseDialog)}/>
-        </ScrollArea>
-        <DrawerFooter className="pt-2">
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+        }
+      </DialogContent>
+    </Dialog>
   )
 }
--- src/app/app/recipes/RecipeGrid.tsx
+++ src/app/app/recipes/RecipeGrid.tsx
@@ -1,3 +1,5 @@
+"use server"
+
 import React from 'react'
 import { validateRequest } from "@/lib/auth";
 import { redirect } from "next/navigation";
