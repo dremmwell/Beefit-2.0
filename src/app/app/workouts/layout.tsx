49cec4d775541ddbd5c8ff33c1a6992fa7ce8b30
dremmwell <dremmwell4>
2026-03-28 12:58:13 +0000

    skeleton workout

--- src/app/app/workouts/layout.tsx
+++ src/app/app/workouts/layout.tsx
@@ -2,6 +2,8 @@ import { validateRequest } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import type React from "react"
 import NavTabs from "../../../components/nav-tab"
+import { Suspense } from "react";
+import FocusSkeleton from "./focus/FocusSkeleton";
 
 const workoutTabs = [
   { label: "Focus", href: "/app/workouts/focus" },
@@ -23,13 +25,15 @@ export default async function Layout({
     
     return (
       <div className="container sm:my-10 my-2 flex flex-col max-h-fit min-h-0 px-3 sm:px-10">
-        <div className="flex lg:gap-2 flex-col sm:flex-row items-start border-b pb-2 lg:pb-0">
-          <h1 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 mb-1">Workouts</h1>
+        <div className="flex lg:gap-2 flex-col sm:flex-row items-start border-b pb-2">
+          <h1 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">Workouts</h1>
           <NavTabs tabs={workoutTabs} className="w-full lg:w-1/3" />
         </div>
         <div className="flex flex-col max-h-fit min-h-0 mt-4 lg:mt-6">
-          {children}
+          <Suspense fallback={<FocusSkeleton />}>
+            {children}
+          </Suspense>
         </div>
       </div>
     )
-  }
+}
