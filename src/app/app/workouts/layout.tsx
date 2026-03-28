b76b9e97fdd9c359ca4b7d09f987e5b15fa0b7be
dremmwell <dremmwell4>
2026-03-28 12:32:31 +0000

    workout tabs

--- src/app/app/workouts/layout.tsx
+++ src/app/app/workouts/layout.tsx
@@ -23,8 +23,10 @@ export default async function Layout({
     
     return (
       <div className="container sm:my-10 my-2 flex flex-col max-h-fit min-h-0 px-3 sm:px-10">
-        <h1 className="scroll-m-20 border-b text-3xl font-semibold tracking-tight first:mt-0 mb-2">Workouts</h1>
-        <NavTabs tabs={workoutTabs} className="w-full lg:w-1/3" />
+        <div className="flex lg:gap-2 flex-col sm:flex-row items-start border-b pb-2 lg:pb-0">
+          <h1 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 mb-1">Workouts</h1>
+          <NavTabs tabs={workoutTabs} className="w-full lg:w-1/3" />
+        </div>
         <div className="flex flex-col max-h-fit min-h-0 mt-4 lg:mt-6">
           {children}
         </div>
